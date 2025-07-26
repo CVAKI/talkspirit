ffad3ae8565ca6018472fa8bba43a01baaedbc8d
SIVANAND <sivanand2004@gmail.com>
2025-07-27 00:08:55 +0530

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -1,25 +1,12 @@
-// js/coin.js
-import { auth, db, onAuthStateChanged, doc, getDoc, updateDoc } from "./firebase-config.js";
+import { auth } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const coinDisplay = document.getElementById("coinCount");
-const startBtn = document.getElementById("startChatBtn");
-
-onAuthStateChanged(auth, async user => {
-  if (!user) window.location.href = "index.html";
-
-  const userRef = doc(db, "users", user.uid);
-  const userDoc = await getDoc(userRef);
-  let coins = userDoc.data().coins;
-  coinDisplay.textContent = coins;
-
-  startBtn.onclick = async () => {
-    if (coins >= 1000) {
-      coins -= 1000;
-      await updateDoc(userRef, { coins });
-      alert("Matched with random user! (fake for now)");
-      coinDisplay.textContent = coins;
-    } else {
-      alert("Not enough Spirit Coins!");
-    }
-  };
+onAuthStateChanged(auth, (user) => {
+  const userInfoEl = document.getElementById("userInfo");
+  if (user) {
+    const nameOrEmail = user.displayName || user.email;
+    userInfoEl.textContent = `Logged in as: ${nameOrEmail}`;
+  } else {
+    userInfoEl.textContent = "Not logged in";
+  }
 });
