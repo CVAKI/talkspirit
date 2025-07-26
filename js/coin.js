ffb6991dbedbc478198e4c44c21e0bea63867c04
SIVANAND <sivanand2004@gmail.com>
2025-07-27 00:13:49 +0530

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -1,3 +1,28 @@
+
+import { auth, db, onAuthStateChanged, doc, getDoc, updateDoc } from "./firebase-config.js";
+
+const coinDisplay = document.getElementById("coinCount");
+const startBtn = document.getElementById("startChatBtn");
+
+onAuthStateChanged(auth, async user => {
+  if (!user) window.location.href = "index.html";
+
+  const userRef = doc(db, "users", user.uid);
+  const userDoc = await getDoc(userRef);
+  let coins = userDoc.data().coins;
+  coinDisplay.textContent = coins;
+
+  startBtn.onclick = async () => {
+    if (coins >= 1000) {
+      coins -= 1000;
+      await updateDoc(userRef, { coins });
+      alert("Matched with random user! (fake for now)");
+      coinDisplay.textContent = coins;
+    } else {
+      alert("Not enough Spirit Coins!");
+    }
+  };
+  //--------------------------------------------------------------------------------------------------------------2
 import { auth } from "./firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
